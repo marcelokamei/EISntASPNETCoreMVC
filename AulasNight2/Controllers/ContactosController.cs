bcc11d82cd5b7a9c60c80b1cae5e5645b2d16593
marcelokamei <marcelokamei@hotmail.com>
2025-02-10 20:26:52 +0000

    Adicionar arquivos de projeto.

---
+++ AulasNight2/Controllers/ContactosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AulasNight2.Controllers
+{
+    public class ContactosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AulasNight2/Controllers/GestaoProdutosController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AulasNight2.Controllers
+{
+    public class GestaoProdutosController : Controller
+    {
+
+        //--- AÇÕES RELACIONADAS AOS PRODUTOS
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+
+
+
+        //--- AÇÕES RELACIONADAS ÀS CATEGORIAS
+        public IActionResult Categorias()
+        {
+            return View();
+        }
+
+        public IActionResult CriarCategoria()
+        {
+            return View();
+        }
+
+        public IActionResult EditarCategoria()
+        {
+            return View();
+        }
+
+
+        //--- AÇÕES RELACIONADAS AOS FORNECEDORES
+        public IActionResult Fornecedores()
+        {
+            return View();
+        }
+
+        public IActionResult CriarFornecedores()
+        {
+            return View();
+        }
+
+        public IActionResult EditarFornecedor()
+        {
+            return View();
+        }
+
+    }
+}
