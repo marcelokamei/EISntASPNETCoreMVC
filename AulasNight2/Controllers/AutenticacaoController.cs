b754375138d50a5f3e25a3053b663d454afdc0d2
marcelokamei <marcelokamei@hotmail.com>
2025-03-18 09:48:58 +0000

    Adicionado script modal para autenticação no site

---
+++ AulasNight2/Controllers/AutenticacaoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AulasNight2.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        public IActionResult VerificarCredenciais( string username, string password)
+        {
+            // simulacao de verificacao(substituir por chamada ao banco de dados)
+            if (username == "admin" && password == "admin")
+            {
+                return Json(new {success = true}); //retorna um json com a propriedade sucess = true
+            }
+            else
+            {
+                return Json(new { success = false }); //retorna um json com a propriedade sucess = false
+
+            }
+        }
+    }
+}
